46376076a7d188f9d4a15dbed548a1dfa2a2014c
Cyphaar~ <victorokafor500@yahoo.com>
2024-12-10 12:06:46 +0100

    new changes

--- src/app/transaction-history/page.tsx
+++ src/app/transaction-history/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
   return (
     <div>
-      
+      <h2>Transaction History</h2>
     </div>
   )
 }
 
-export default page
+export default page;
